a0c0b6b6331606f3c0400721473abe9c845bac2c
Luis Rey Lara <luisreylara@hotmail.com>
2025-06-25 05:57:57 -0600

    video -18

---
+++ src/modules/common/components/BotonFlotante.vue
@@ -0,0 +1,11 @@
+<template>
+    <button class="btn btn-circle btn-secondary fixed bottom-10 right-10">
+        <slot />
+    </button>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped></style>
